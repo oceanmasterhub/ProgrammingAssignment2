a8e95764bbdd8c6ed7fcc0ce437efb8794c22783
L. Q-G <63164232+oceanmasterhub@users.noreply.github.com>
2020-04-16 04:40:50 +0100

    Updated cachematrix.R
    Programming Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## -------------
+## cachematrix.R
+## -------------
+## This file includes functions to speed up computation of the inverse of a
+## matrix by means of a cache version stored in the environment
 
-## Write a short comment describing this function
+## -----------------
+## makeCacheMatrix()
+## -----------------
+## This function creates a special list object with functions that will help
+## cacheSolve() to retrieve the inverse of matrix 'x' from cache (if exists)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv     <- NULL
+  set_mat <- function(y) {
+    x    <<- y
+    inv  <<- NULL
+  }
+  get_mat <- function() x
+  set_inv <- function(inverse) inv <<- inverse
+  get_inv <- function() inv
+  
+  list(set_mat = set_mat, # Create object as list
+       get_mat = get_mat,
+       set_inv = set_inv,
+       get_inv = get_inv)
 }
 
-
-## Write a short comment describing this function
+## ------------
+## cacheSolve()
+## ------------
+## This function retrieves the inverse of a matrix from the cache if available
+## and execute the solve() function instead when cache is empty
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inv()
+  if(!is.null(inv)) {
+    message("Getting cached data...")
+    return(inv)
+  }
+  
+  data <- x$get_mat()
+  inv <- solve(data, ...)
+  x$set_inv(inv)
+  inv
 }
